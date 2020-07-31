558fd981afa8d2c3f63fca08c2c6fda51dcb27e7
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2020-07-31 11:43:37 +0900

    complete

---
+++ src/routes/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+	return (
+		<div className="makecenter">
+			<div className="about__container">
+				<span>
+					“Freedom is the freedom to say that two plus two make four. If that is
+					granted, all else follows.”
+				</span>
+				<span>− George Orwell, 1984</span>
+			</div>
+		</div>
+	);
+}
+
+export default About;
